c65ffa0cfb43be30d0330359d702237d37887f3d
Mo <morahman97@gmail.com>
2025-02-03 21:18:24 -0800

    Add MUI, date picker, mood select, and text field

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,83 @@
-import { createRoot } from 'react-dom/client';
+import { Component, useState } from 'react'
+import { Container, createTheme, Menu, ThemeProvider } from '@mui/material'
+import Button from '@mui/material/Button'
+import { Input, TextField } from '@mui/material'
 
-// const root = createRoot(document.body);
-// root.render(
-//     <h2>Hello from React!</h2>
-// );
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+import CssBaseline from "@mui/material/CssBaseline";
+
+// import * from 'fs';
+
+import './App.css'
 
 function App() {
-    return(<h1>this is some content</h1>)
+  const [count, setCount] = useState(0)
+  const [mood, setMood] = useState(String)
+  
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+      text: {
+        primary: "#ffffff"
+      }
+    }
+  })
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setMood(event.target.value as string);
+  };
+
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline /> {/* Ensures dark theme applies to the entire app */}
+      <h1>Mo's Personal Journal App</h1>
+
+      {/* Add section showing today's date */}
+      <h3>Choose date for journal entry</h3>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker defaultValue={dayjs(new Date())}/>
+      </LocalizationProvider> 
+
+      {/* Add section for mood for today on top, three faces with varying moods to indicate happy, meh, bad */}
+      <h3>How did you feel today overall?</h3>
+      <FormControl>
+        <Select
+          value={mood}
+          onChange={handleChange}
+          // renderValue={(selected) => (selected ? selected : "select an option")} // doesnt work
+        >
+          <MenuItem value={"good"}>Good</MenuItem>
+          <MenuItem value={"neutral"}>Neutral</MenuItem>
+          <MenuItem value={"bad"}>Bad</MenuItem>
+        </Select>
+      </FormControl>
+
+      {/* Section for journal text input */}
+      <TextField 
+        placeholder='Tell me what happened today!' 
+        multiline 
+        fullWidth>
+      </TextField>
+
+      {/* Section for button to submit journal entry */}
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
+          Submit Journal Entry
+        </Button>
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App
