f5cc249c63e07933bc41ec0b28a380324cba33cf
Mo Rahman <morahman97@gmail.com>
2025-02-03 17:21:48 -0800

    properly integrate react

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import App from './app';
 
-const root = createRoot(document.body);
-root.render(
-    <App />
-);
+// const root = createRoot(document.body);
+// root.render(
+//     <App />
+// );
+
+// document.body.innerHTML = '<div id="root"></div>';
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
--- src/renderer.ts
+++ src/renderer.ts
@@ -25,9 +25,8 @@
  *  });
  * ```
  */
-// Add this to the end of the existing file
-import './main';
-
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
+// Add this to the end of the existing file
+import './main';
